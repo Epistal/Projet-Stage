a1e57711ee97c00592640bfa2576444403a92406
Téo Callaris <teo.callaris@epitech.eu>
2018-07-03 11:41:38 +0200

    Placement des boutons

--- Epistal/App.js
+++ Epistal/App.js
@@ -5,10 +5,47 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={()=>{alert("you clicked me")}}>
-          <Image style={{width: 50, height: 50}} source={require("./assets/icone.png")}/>
-        </TouchableOpacity>
-        <Text>We are just getting started</Text>
+      
+        <View style={styles.row}>
+        
+          <TouchableOpacity style={styles.button} onPress={()=>{alert("Lumière")}}>
+            <Image style={{width: 50, height: 50}} source={require("./assets/ampoule.png")}/>
+          </TouchableOpacity>
+          
+          <TouchableOpacity style={styles.button} onPress={()=>{alert("Piscine")}}>
+            <Image style={{width: 50, height: 50}} source={require("./assets/piscine.png")}/>
+          </TouchableOpacity>
+          
+          <TouchableOpacity style={styles.button} onPress={()=>{alert("Alarme")}}>
+            <Image style={{width: 50, height: 50}} source={require("./assets/alarm.png")}/>
+          </TouchableOpacity>
+          
+        </View>
+        
+        <View style={styles.row}>
+
+          <TouchableOpacity style={styles.button} onPress={()=>{alert("Store")}}>
+            <Image style={{width: 50, height: 50}} source={require("./assets/volets.png")}/>
+          </TouchableOpacity>
+          
+          <TouchableOpacity style={styles.button} onPress={()=>{alert("Arrosage")}}>
+            <Image style={{width: 50, height: 50}} source={require("./assets/tuyau2.png")}/>
+          </TouchableOpacity>
+          
+
+        </View>
+        
+        <View style={styles.row}>
+
+          <TouchableOpacity style={styles.button} onPress={()=>{alert("Clim")}}>
+            <Image style={{width: 50, height: 50}} source={require("./assets/clim.png")}/>
+          </TouchableOpacity>
+          
+          <TouchableOpacity style={styles.button} onPress={()=>{alert("Accès")}}>
+            <Image style={{width: 50, height: 50}} source={require("./assets/key.png")}/>
+          </TouchableOpacity>
+
+        </View>
       </View>
     );
   }
@@ -17,15 +54,18 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'baseLine',
+    justifyContent: 'space-between',
+    marginTop: 60,
+    margin: 40,
   },
   button: {
-    backgroundColor: '#859a9b',
+    backgroundColor: '#048B9A',
     borderRadius: 20,
-    padding: 10,
-    marginBottom: 20,
+    padding: 15,
+    marginBottom: 45,
     shadowColor: '#303838',
     shadowOffset: { width: 0, height: 5 },
     shadowRadius: 10,
