7bb82b0dbff1c8895a39e19f8f4b81fbe56653d8
Téo Callaris <teo.callaris@epitech.eu>
2018-07-04 16:55:17 +0200

    picker

--- Epistal/components/eclairage.js
+++ Epistal/components/eclairage.js
@@ -18,28 +18,41 @@ export default class App extends React.Component {
    return (
       
       <View style={styles.container}>
-
+      
         <View style={styles.row}>
-          <TouchableOpacity style={styles.button}  onPress={() => {alert("lol")}}>
+          <TouchableOpacity style={styles.button}  onPress={() => {alert("Pieces")}}>
             <Image style={{width: 50, height: 50}} source={require("../assets/piece.png")}/>
           </TouchableOpacity>
-        </View>
-        
-        <View style={styles.row}>
-          <TouchableOpacity style={styles.button} onPress={()=>{alert("Store")}}>
-            <Image style={{width: 50, height: 50}} source={require("../assets/expo.symbol.white.png")}/>
-          </TouchableOpacity>
+    
         </View>
         
         <View style={styles.row}>
            <Picker
-          style={{width: 100}}
+          style={{width: 110, marginTop: 160}}
           selectedValue={this.state.language}
           onValueChange={(lang) => this.setState({language: lang})}>
-          <Picker.Item label="Java" value="java" />
-          <Picker.Item label="JavaScript" value="js" />
+          <Picker.Item label="Salon" value="salon" />
+          <Picker.Item label="Cuisine" value="cuisine" />
+          <Picker.Item label="Chambre 1" value="chambre 1" />
+          <Picker.Item label="Chambre 2" value="chambre 2" />
+          <Picker.Item label="Chambre 3" value="chambre 3" />
+          <Picker.Item label="Bain-WC" value="bain-wc" />
+          <Picker.Item label="Couloirs" value="couloirs" />
+          <Picker.Item label="Jardin" value="jardin" />
+          <Picker.Item label="Garage" value="garage" />
+          <Picker.Item label="Piscine" value="piscine" />
         </Picker>
+            <TouchableOpacity style={styles.buttonok} onPress={()=>{alert(this.state.language)}}>
+            <Image style={{width: 50, height: 50}} source={require("../assets/valider.png")}/>
+          </TouchableOpacity>      
         </View>
+        
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.button} onPress={()=>{alert(" ")}}>
+            <Image style={{width: 50, height: 50}} source={require("../assets/script.png")}/>
+          </TouchableOpacity>
+        </View>
+        
         
       </View>
     );
@@ -51,7 +64,6 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     backgroundColor: '#E9E8EE',
-    alignItems: 'baseLine',
     justifyContent: 'space-between',
     marginTop: 60,
     margin: 55,
@@ -60,7 +72,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#048B9A',
     borderRadius: 20,
     padding: 15,
-    marginBottom: 45,
+    marginBottom: 230,
+    shadowColor: '#303838',
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 10,
+    shadowOpacity: 0.35,
+  },
+  buttonok: {
+    backgroundColor: '#048B9A',
+    borderRadius: 20,
+    padding: 15,
+    marginTop: 20,
+    marginBottom: 230,
+    marginLeft: 15,
+    marginRight: 20,
     shadowColor: '#303838',
     shadowOffset: { width: 0, height: 5 },
     shadowRadius: 10,
